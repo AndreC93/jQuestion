0a66ec86a755202faba054c1268e7f416d35d628
Andre <fendred@yahoo.com>
2018-09-25 19:50:01 -0400

    Add Text Speed folder

---
+++ type_speed/text_speed.js
@@ -0,0 +1,18 @@
+const quotes = [
+  "I solemnly swear that I am up to no good.",
+  "Fear of a name increases fear of the thing itself.",
+  "Time will not slow down when something unpleasant lies ahead.",
+  "Just because you have the emotional range of a teaspoon doesn't mean we all have.",
+  "It does not do to dwell on dreams and forget to live.",
+  "Don't let the muggles get you down.",
+  "What's comin' will come, and we'll meet it when it does.",
+  "Once again, you show all the sensitivity of a blunt axe.",
+  "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.",
+  "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals."
+];
+
+const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+document.addEventListener("DOMContentLoaded", () => {
+  $j('.main')
+});
