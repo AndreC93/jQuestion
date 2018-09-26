9fc4efcf307c8955430068317587b66f91dd0a40
Andre <fendred@yahoo.com>
2018-09-25 21:09:08 -0400

    Add HTML elements to text speed test

--- type_speed/text_speed.js
+++ type_speed/text_speed.js
@@ -11,8 +11,39 @@ const quotes = [
   "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals."
 ];
 
-const quote = quotes[Math.floor(Math.random() * quotes.length)];
+generateRandomIndex = () => Math.floor(Math.random() * quotes.length);
+
+const quote = quotes[generateRandomIndex()];
+
+let startTime;
+let endTime;
+let errorMessage = '';
 
 document.addEventListener("DOMContentLoaded", () => {
-  $j('.main')
+  $j('.main').append('<div><h1>Welcome to Text Speed Test</h1><h2></h2></div>')
+  $j('.main').find('h2').html(quote).append('<br>').append("<textarea rows='6' cols='80' class='text-box' >");
+  $j('.text-box').on('keypress', (e) => handleKeypress(e));
+  $j('.main').append(`<p>${generateTimeScore(startTime)}</p>`)
+
 });
+
+handleKeypress = (e) => {
+  if (startTime === undefined) {
+    startTime = new Date;
+    setInterval(() => $j('p').html(`${generateTimeScore(startTime)}`, 1000));
+  } else if (e.target.value === quote && endTime === undefined) {
+    endTime = new Date;
+    generateTimeScore(startTime, endTime);
+  } else if (e.target.value.length >= quote.length) {
+    errorMessage = 'Incorrect Quote';
+  } else if (e.target.value.length === quote.length) {
+    errorMessage = '';
+  }
+};
+
+generateTimeScore = (start, end = (new Date)) => {
+  if (start === undefined) return 'Time will start when you start typing.';
+  const diff = (end - start)/1000;
+  const seconds = Math.round(diff);
+  return `${seconds} seconds`;
+};
