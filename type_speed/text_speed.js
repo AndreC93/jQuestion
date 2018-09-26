22473c9cb7ce8c9d0660dccc8889250a0fb59a47
Andre <fendred@yahoo.com>
2018-09-25 23:10:51 -0400

    Finish styling Live page

--- type_speed/text_speed.js
+++ type_speed/text_speed.js
@@ -11,38 +11,73 @@ const quotes = [
   "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals."
 ];
 
-generateRandomIndex = () => Math.floor(Math.random() * quotes.length);
+const generateRandomIndex = () => Math.floor(Math.random() * quotes.length);
+let pastIndex;
 
-const quote = quotes[generateRandomIndex()];
+const pickAQuote = () => {
+  if (pastIndex === undefined) {
+    pastIndex = generateRandomIndex();
+  } else {
+    let newIndex = generateRandomIndex();
+    while (newIndex === pastIndex) {
+      newIndex = generateRandomIndex();
+    }
+    pastIndex = newIndex;
+  }
+  return quotes[pastIndex];
+};
+
+let quote = pickAQuote();
+
+
 
 let startTime;
 let done = false;
-let errorMessage = '';
 let interval;
 
 document.addEventListener("DOMContentLoaded", () => {
-  $j('.main').append('<div><h1>Welcome to Text Speed Test</h1><h2></h2></div>')
-  $j('.main').find('h2').html(quote).append('<br>').append("<textarea rows='6' cols='80' class='text-box' >");
-  $j('.main').append(`<p>${generateTimeScore(startTime)}</p>`)
+  $j('html').attr('style', "background: url('https://www.textrazor.com/img/letters_inverse.png')");
+  $j('body').attr('style', 
+    "background: white; margin: 100px auto; text-align: center; width: 700px; padding: 20px;");
+  $j('.main').append('<div><h1>Welcome to Text Speed Test</h1><span></span><h2></h2><section/></div>');
+  $j('span').html('Type out the quote below and see how fast you can do it without typos.')
+  $j('span').attr('style', 'font-style: italic;')
+  $j('h2').html(quote);
+  $j('section').append('<br>').append("<textarea rows='6' cols='80' >");
+  $j('.main').append(`<p>${generateTimeScore(startTime)}</p>`).append(`<h3 style='color: red' ></h3>`);
+  $j('.main').append('<button>Reset Text</button>');
   $j('textarea').on('keypress', ((e) => handleKeypress(e)));
+  $j('textarea').on('keydown', ((e) => handleKeypress(e)));
+  $j('button').on('click', () => handleClick())
+  $j('button').attr('style', 'padding: 6px 10px; color: brown; background: azure; font-weight: bolder;');
 });
 
-handleKeypress = (e) => {
+const handleKeypress = (e) => {
   if (startTime === undefined) {
     startTime = new Date;
     interval = setInterval(() => $j('p').html(`${generateTimeScore(startTime)}`, 1000));
   } else if (e.target.value + e.key === quote && done === false) {
     clearInterval(interval);
     done = true;
-    $j('p').attr('style', 'color: green')
+    $j('p').attr('style', 'color: green');
   } else if (e.target.value.length >= quote.length) {
-    $j('p').attr('style', 'color: red')
-  } else if (e.target.value.length === quote.length) {
-    $j('p').attr('style', 'color: black')
+    $j('p').attr('style', 'color: red');
+    $j('h3').html('Invalid Text!!!');
+  } else if (e.target.value.length <= quote.length) {
+    $j('p').attr('style', 'color: black');
+    $j('h3').html('');
   }
 };
 
-generateTimeScore = (start, end = (new Date)) => {
+const handleClick = () => {
+  quote = pickAQuote();
+  $j('h2').html(quote);
+  startTime = undefined;
+  done = false;
+  setTimeout(() => clearInterval(interval), 1000);
+};
+
+const generateTimeScore = (start, end = (new Date)) => {
   if (start === undefined) return 'Time will start when you start typing.';
   const diff = (end - start)/1000;
   const seconds = Math.round(diff);
